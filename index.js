b3091a8f08be321a5ef63aaaa120aa0f7d03ea32
Rami B <ramibasheer9@gmail.com>
2022-09-20 17:11:04 +0200

    apply solution

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const parentsList = [];
+
+let currentElement = document.querySelector('cite');
+
+let currentElementName = currentElement.nodeName;
+parentsList.unshift(currentElementName);
+currentElement = currentElement.parentElement;
+
+
+while(currentElement) {
+    let currentElementName = currentElement.nodeName;
+    parentsList.unshift(currentElementName.toLowerCase());
+    currentElement = currentElement.parentElement;
+}
+
+console.log(parentsList.join(' > '));
+
